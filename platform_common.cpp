b69460e669e3333921b9d23009e7b6b7b5f16476
sm1led <65720217+sm1led@users.noreply.github.com>
2020-05-22 12:04:09 +0300

    Add files via upload
    Galutinis zaidimas.

---
+++ platform_common.cpp
@@ -0,0 +1,23 @@
+struct Button_State 
+{
+	bool is_down;
+	bool changed;
+};
+
+enum
+{
+	BUTTON_UP,
+	BUTTON_DOWN,
+	BUTTON_W,
+	BUTTON_S,
+	BUTTON_LEFT,
+	BUTTON_RIGHT,
+	BUTTON_ENTER,
+
+	BUTTON_COUNT, // LAST ITEM
+};
+
+struct Input
+{
+	Button_State buttons[BUTTON_COUNT];
+};
